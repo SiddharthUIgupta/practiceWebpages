7e0a697385f1abb23f570b249f5573ee741e9535
Siddharth <siddharthgupta3391@gmail.com>
2017-09-15 14:58:07 -0700

    last commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function(){
+	jQuery('#slider').skdslider({
+		'delay':5000, 
+		'animationSpeed': 2000,
+		'showNextPrev':true,
+		'showPlayButton':true,
+		'autoSlide':true,
+		'animationType':'fading'
+	});
+});
